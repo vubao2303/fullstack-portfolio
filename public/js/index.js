dcf4f4cd4e6031165d0e15abf906ca9552d2338e
vubao2303 <bbbvut@gmail.com>
2021-02-27 22:09:59 -0800

    add react projects

--- public/js/index.js
+++ public/js/index.js
@@ -11,38 +11,3 @@ $(document).ready(function () {
     window.location.replace("https://github.com/vubao2303/big-burger")
   })
 })
-
-
-// function allProjects(input) {
-//   for (var i = 0; i < input.length; i++) {
-//     var html = `<li class="list-group-item">${input[i].title}
-//   <button
-//     class="li-btn"
-//     type="button"
-//     data-toggle="collapse"
-//     data-target="#all-${input[i].id}"
-//     aria-expanded="false"
-//     aria-controls="inputInfo-allParties"
-//     >View Party</button>
-//   </li>
-
-//   <div class="collapse" id="all-${input[i].id}">
-//     <div class="card card-body dropdown">
-
-//       <!-- <p class="host-name">Host:{$input[i].name}</p> -->
-
-
-//       <p>Theme: ${input[i].theme}</p>
-//       <p>Location: ${input[i].location}</p>
-//       <p>Date: ${moment(input[i].date).format("dddd, MMMM Do YYYY")}</p>
-//       <p>Time: ${input[i].time}</p>
-//       <hr />
-//       <button id=${input[i].id} class="attend-btn li-btn" data-id=${
-//       input[i].id
-//     }>See live site</button>
-//     </div>
-//   </div>`;
-
-//     $(".allEvents-list").append(html);
-//   }
-// }
